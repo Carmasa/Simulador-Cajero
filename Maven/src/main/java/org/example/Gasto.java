cd81d1a93c8b0378a9344b0b5bebb5b16c734a87
Carmasa <zamarra107@gmail.com>
2025-04-24 13:05:41 +0200

    Añadido Maven

---
+++ Maven/src/main/java/org/example/Gasto.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//Definición de la clase gastos, hereda de la clase abstracta dinero.
+public class Gasto extends Dinero {
+
+    public Gasto(double gasto, String descipcion) {
+        super(gasto, descipcion); // Llama a los atributos del padre
+    }
+
+    @Override
+    public String toString() {
+        return "Gasto realizado: " + dinero + "\nDescripcion del gasto: " + descripcion;
+    }
+
+}
---
+++ Maven/src/main/java/org/example/Ingreso.java
@@ -0,0 +1,15 @@
+package org.example;
+
+    //Definición de la clase ingresos, hereda de la clase abstracta dinero.
+    public class Ingreso extends Dinero {
+
+        public Ingreso(double ingreso, String descipcion) {
+            super(ingreso, descipcion);
+        }
+
+        @Override
+        public String toString() {
+            return "Dinero ingresado: " + dinero + "\nDescripcion del ingreso: " + descripcion;
+        }
+
+    }
